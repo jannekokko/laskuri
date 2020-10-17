b8ba232bc747f83accb07fc6f158cb5d1fa502f2
Janne <Janne@DESKTOP-624TQ02>
2020-10-17 17:25:19 +0300

    commit 4

--- src/main/java/laskuri/Laskuri.java
+++ src/main/java/laskuri/Laskuri.java
@@ -5,32 +5,39 @@ package laskuri;
 public class Laskuri{
   /* laskuriolion kentan arvoa voi kasvattaa yhdella kutsumalla metodia
      lisaa(). Arvoa voi tarkastella ja sen voi nollata.                 */
-	private int tapahtumia;
-        public Laskuri(int alkuarvo){ 
-        // konstruktori, kuormitettu. Alkuarvon tulee olla >= 0.
-		if(alkuarvo < 0)
-			return;
-		this.tapahtumia = alkuarvo;
-	}
-	public Laskuri(){
-		this.tapahtumia = 0;
-	}
+    private int tapahtumia;
+    public Laskuri(int alkuarvo){ 
+    // konstruktori, kuormitettu. Alkuarvon tulee olla >= 0.
+        if(alkuarvo < 0)
+                return;
+        this.tapahtumia = alkuarvo;
+    }
+    public Laskuri(){
+        this.tapahtumia = 0;
+    }
 
-  	public void lisaa(){
-    		this.tapahtumia = this.tapahtumia + 1; 
-  	}
-        public void lisaa(int lisays) {
-            this.tapahtumia = this.tapahtumia + lisays;
-        }
-  	public int lukema(){
-    		return this.tapahtumia;
-  	}
-  	public void nollaa(){
-    		this.tapahtumia = 0;
-  	}
-	public String toString(){
-		return "Lukema: " + this.tapahtumia;
-	}
+    public void lisaa(){
+        this.tapahtumia = this.tapahtumia + 1; 
+    }
+    public void lisaa(int lisays) {
+        this.tapahtumia = this.tapahtumia + lisays;
+    }
+    public int lukema(){
+        return this.tapahtumia;
+    }
+    public void nollaa(){
+        this.tapahtumia = 0;
+    }
+    public String toString(){
+        return "Lukema: " + this.tapahtumia;
+    }
+        
+    public static void main(String[] args) {
+        Laskuri laskurini = new Laskuri(5);
+        System.out.println(laskurini);
+
+
+    }
 
 }
 
